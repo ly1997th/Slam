ae7be508d723f7759c48dbac286fddbaa578e75e
ly1997th <ly_dra@163.com>
2018-03-09 12:42:38 +0800

    modify usual_cmake add ch6

---
+++ slam14/ch6/g2o_curve_fitting/build/CMakeFiles/curve_fitting.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ly_dra/Desktop/Slam/slam14/ch6/g2o_curve_fitting/g2o_curve_fitting.cpp" "/home/ly_dra/Desktop/Slam/slam14/ch6/g2o_curve_fitting/build/CMakeFiles/curve_fitting.dir/g2o_curve_fitting.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ slam14/ch6/g2o_curve_fitting/g2o_curve_fitting.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/core/optimization_algorithm_dogleg.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#include <Eigen/Core>
+#include <opencv2/core/core.hpp>
+#include <cmath>
+#include <chrono>
+
+using namespace std;
+
+class CurveFittingVertex:public g2o::BaseVertex<3,Eigen::Vector3d>
+{
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW				//eigen进行对齐
+	virtual void setToOriginImpl()    			//重置
+	{
+		_estimate<<0,0,0;
+	}
+	virtual void oplusImpl(const double *update)//更新
+	{
+		_estimate+=Eigen::Vector3d(update);
+	}
+	//存盘和读盘，留空
+	virtual bool read(istream &in){}
+	virtual bool write(ostream &out) const {}
+};
+//误差模板模型参数：观测值维度、类型、连接顶点类型
+class CurveFittingEdge:public g2o::BaseUnaryEdge<1,double,CurveFittingVertex>
+{
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+	CurveFittingEdge(double x):BaseUnaryEdge(),_x(x){}
+	//calculate the residual of the model
+	void computeError()
+	{
+		const CurveFittingVertex *v=static_cast<const CurveFittingVertex *>(_vertices[0]);
+		const Eigen::Vector3d abc=v->estimate();
+		_error(0,0) =_measurement-std::exp(abc(0,0)*_x*_x+abc(1,0)*_x+abc(2,0));
+	}
+	virtual bool read(istream &in){}
+	virtual bool write(ostream &out) const {}
+public:
+	double _x;								//x value;y value is _measurement
+};
+
+int main(int argc, char** argv)
+{
+	double a=1.0,b=2.0,c=1.0;
+	int N=100;
+	double w_sigma=1.0;
+	cv::RNG rng;
+	double abc[3]={0,0,0};
+
+	vector<double>x_data,y_data;
+	cout<<"generating data:"<<endl;
+	for(int i=0;i<N;++i)
+	{
+		double x=i/100.0;
+		x_data.push_back(x);
+		y_data.push_back(exp(a*x*x+b*x+c)+rng.gaussian(w_sigma));
+		cout<<x_data[i]<<y_data[i]<<endl;
+	}
+	//构建图优化，先设定g20
+	//矩阵块：每个误差项优化变量维度为3，误差维度为1
+// 	typedef g2o::BlockSolver<g2o::BlockSolverTraits<3,1> >Block;
+// 	Block::LinearSolverType* linearSolver(new g2o::LinearSolverDense<Block::PoseMatrixType>());
+// 	Block* solver_ptr=new Block(std::unique_ptr<Block::LinearSolverType>(linearSolver));
+// 	g2o::OptimizationAlgorithmLevenberg* solver=new g2o::OptimizationAlgorithmLevenberg(std::move(solver_ptr));
+// //	g2o::OptimizationAlgorithmGaussNewwton* solver=new g2o::OptimizationAlgorithmGaussNewton(solver_ptr);
+// //	g2o::OptimizationAlgorithmGogleg* solver=new g2o::OptimizationAlgorithmDogleg(solver_ptr);
+// 	g2o::SparseOptimizer optimizer;
+// 	optimizer.setAlgorithm(solver);
+// 	optimizer.setVerbose(true);
+
+
+	typedef g2o::BlockSolver< g2o::BlockSolverTraits<3,1> > Block;
+	Block::LinearSolverType* linearSolver = new g2o::LinearSolverDense<Block::PoseMatrixType>();
+	Block* solver_ptr = new Block( std::unique_ptr<Block::LinearSolverType>(linearSolver) );
+	g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(std::unique_ptr<Block>(solver_ptr) );
+	g2o::SparseOptimizer optimizer;   
+	optimizer.setAlgorithm( solver );   
+	optimizer.setVerbose( true ); 
+
+	CurveFittingVertex* v=new CurveFittingVertex();
+	v->setEstimate(Eigen::Vector3d(0,0,0));
+	v->setId(0);
+	optimizer.addVertex(v);
+	//向图中增加边
+	for(int i=0;i<N;++i)
+	{
+		CurveFittingEdge* edge=new CurveFittingEdge(x_data[i]);
+		edge->setId(i);
+		edge->setVertex(0,v);
+		edge->setMeasurement(y_data[i]);
+		edge->setInformation(Eigen::Matrix<double,1,1>::Identity()*1/(w_sigma*w_sigma));
+		optimizer.addEdge(edge);
+	}
+
+	//执行优化
+	cout<<"start optimization"<<endl;
+	chrono::steady_clock::time_point t1=chrono::steady_clock::now();
+	optimizer.initializeOptimization();
+	optimizer.optimize(100);
+	chrono::steady_clock::time_point t2=chrono::steady_clock::now();
+	chrono::duration<double> time_used=chrono::duration_cast<chrono::duration<double>>(t2-t1);
+	cout<<"solve time cost="<<time_used.count()<<"seconds."<<endl;
+
+	//输出优化值
+	Eigen::Vector3d abc_estimate=v->estimate();
+	cout<<"estimated model:"<<abc_estimate.transpose()<<endl;
+
+	return 0;
+
+}
